4149736b1cf5e6eec0b35317279f7a5c8fa6bfcb
PIYUSH14SHARMA <piyush.sharma14012003@gmail.com>
2025-05-21 01:32:48 +0530

    Add files via upload

---
+++ appchat/src/test/java/com/chat/appchat/AppchatApplication.java
@@ -0,0 +1,10 @@
+package com.chat.appchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppchatApplicationTests {
+	public static void main(String[] args) {SpringApplication.run(AppchatApplication.class, args);}
+
+}
---
+++ appchat/src/test/java/com/chat/appchat/controller/ChatController.java
@@ -0,0 +1,21 @@
+package com.chat.appchat.controller;
+
+import com.chat.appchat.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/sendMessage")
+    @SendTo("/topic/message")
+    public ChatMessage sendMessage(ChatMessage message){
+        return message;
+    }
+    @GetMapping("/chat")
+    public String chat(){
+        return "chat";
+    }
+}
